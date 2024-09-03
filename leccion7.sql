52c9586d62a0ff19f954cdbcb5c2ac955b963bbd
maria <mpramos78915400@gmail.com>
2024-09-02 21:32:30 -0400

    leccion 7 terminado, lista de participantes actualizado

--- leccion7.sql
+++ leccion7.sql
@@ -4,5 +4,12 @@ from edificios as E
 INNER JOIN empleados as Emp
 ON E.Nombre_Del_Edificio=Emp.Edificio
 -- 2. Encuentra la lista de todos los edificios y su capacidad.
-
+select distinct E.nombre_del_Edificio, E.capacidad
+from edificios as E 
+left join Empleados AS Emp
+on E.nombre_del_Edificio=Emp.edificio
 -- 3. Enumere todos los edificios y las distintas funciones de los empleados en cada edificio (incluidos los edificios vacíos)
+select E.nombre_del_Edificio, coalesce(Empl.role,'SIN EMPLEADOS') as Funcion_empleados
+from edificios as E
+left join empleados as Empl ON E.nombre_del_Edificio = Empl.Edificio
+
